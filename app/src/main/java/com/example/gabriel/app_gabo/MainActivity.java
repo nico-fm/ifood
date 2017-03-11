824794cc74188a0a62046cd661d4e94122eb240b
CentralCornet8 <gabasteitor-300@hotmail.com>
2017-03-11 18:23:31 -0400

    Gabo_7

--- app/src/main/java/com/example/gabriel/app_gabo/MainActivity.java
+++ app/src/main/java/com/example/gabriel/app_gabo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.gabriel.app_gabo;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView lblRegistro;
     private Context context;
 
-
+    String nombre_ingresado;
+    String password_ingresado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +29,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         context=this;
+        SharedPreferences prefs = getSharedPreferences("MisPreferencias",Context.MODE_PRIVATE);
 
+        String usuario_almacenado= prefs.getString("usuario","no");
+        String password_almacenado= prefs.getString("password","no");
+
+        if(usuario_almacenado.compareTo("no")!=0 &&password_almacenado.compareTo("no")!=0 ){
+            Intent a=new Intent(getApplicationContext(),MenuPrincipal.class);
+            finish();
+            startActivity(a);
+        }
 
         txtUsuario=(EditText)findViewById(R.id.txtUsuario);
         txtPassword=(EditText)findViewById(R.id.txtPassword);
@@ -36,39 +47,30 @@ public class MainActivity extends AppCompatActivity {
 
         btnIniciar.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                String campo_usuario = txtUsuario.getText().toString();
-                String campo_password = txtPassword.getText().toString();
+                nombre_ingresado = txtUsuario.getText().toString();
+                password_ingresado = txtPassword.getText().toString();
+                SharedPreferences prefs = getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putString("usuario", nombre_ingresado);
+                editor.putString("password", password_ingresado);
+                editor.commit();
 
-                if (campo_usuario.compareTo("gabriel") == 0 && campo_password.compareTo("gabriel") == 0)
-                {
-                    Intent a=new Intent(context,MenuPrincipal.class);
-                    String[] datos=new String[2];
-                    datos[0]=txtUsuario.getText().toString();
-                    datos[1]=txtPassword.getText().toString();
-                    a.putExtra("datos_usuario", datos);
-                    startActivity(a);
+                Toast.makeText(getApplicationContext(), "Usuario" + nombre_ingresado, Toast.LENGTH_SHORT).show();
 
-                }
-                else
-                {
-                            Toast mensaje=Toast.makeText(getApplicationContext(),
-                                    "Usuario o Contrasena Incorrecta", Toast.LENGTH_SHORT);
-                            mensaje.show();
-                }
-            }
-        });
+                Intent a = new Intent(getApplicationContext(),MenuPrincipal.class);
+                startActivity(a);
+                finish();
+            }});
 
         lblRegistro.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view) {
+            public void onClick(View v) {
                 Intent intent = new Intent(context, Registro.class);
                 startActivity(intent);
             }
         });
 
 
-//hola se ve?
+
+    }}
 
 
-
-    }
-}
--- app/src/main/java/com/example/gabriel/app_gabo/MenuPrincipal.java
+++ app/src/main/java/com/example/gabriel/app_gabo/MenuPrincipal.java
@@ -1,12 +1,17 @@
 package com.example.gabriel.app_gabo;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MenuPrincipal extends AppCompatActivity {
 
@@ -18,6 +23,7 @@ public class MenuPrincipal extends AppCompatActivity {
     private Button btnPromociones;
     private Button btnBuscarRestaurant;
     private Button btnFavoritos;
+    private Button btnCerrar;
 
 
     @Override
@@ -33,7 +39,10 @@ public class MenuPrincipal extends AppCompatActivity {
         btnPromociones=(Button)findViewById(R.id.btnPromociones);
         btnBuscarRestaurant=(Button)findViewById(R.id.btnBuscarRestaurant);
         btnFavoritos=(Button)findViewById(R.id.btnFavoritos);
+        btnCerrar=(Button) findViewById(R.id.btnCerrar);
 
+        SharedPreferences prefs = getSharedPreferences("MisPreferencias",Context.MODE_PRIVATE);
+        String usuario_almacenado = prefs.getString("usuario","no");
 
 
         btnComidaRapida.setOnClickListener(new View.OnClickListener() {
@@ -53,10 +62,27 @@ public class MenuPrincipal extends AppCompatActivity {
         btnPromociones.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(context,Promociones.class);
+                Intent intent = new Intent(getApplicationContext(), Promociones.class);
+
+                PendingIntent pIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, 0);
+
+                Notification notificacion = new android.support.v4.app.NotificationCompat.Builder(MenuPrincipal.this)
+                        .setContentTitle("iFood")
+                        .setContentText("Compra la nueva super king!!!")
+                        .setSmallIcon(R.drawable.icon_app)
+                        .setContentIntent(pIntent)
+                        .setDefaults(Notification.DEFAULT_SOUND)
+                        .build();
+
+                NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                notificationManager.notify(0, notificacion);
                 startActivity(intent);
+
             }
         });
+
+
+
         btnBuscarRestaurant.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -72,5 +98,20 @@ public class MenuPrincipal extends AppCompatActivity {
             }
         });
 
+        btnCerrar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getApplicationContext(), "Sesion Cerrada", Toast.LENGTH_SHORT).show();
+                SharedPreferences prefs = getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE);
+
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.clear();
+                editor.commit();
+                Intent a = new Intent(getApplicationContext(), MainActivity.class);
+                finish();
+                startActivity(a);
+            }
+            });
     }
 }
