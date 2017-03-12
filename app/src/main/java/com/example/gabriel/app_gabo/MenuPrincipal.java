e88b80ae961209926ba9e855cd3322acc0249321
CentralCornet8 <gabasteitor-300@hotmail.com>
2017-03-12 19:40:49 -0300

    Gabo_13

--- app/src/main/java/com/example/gabriel/app_gabo/MenuPrincipal.java
+++ app/src/main/java/com/example/gabriel/app_gabo/MenuPrincipal.java
@@ -1,11 +1,14 @@
 package com.example.gabriel.app_gabo;
 
+import android.app.Dialog;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -102,15 +105,28 @@ public class MenuPrincipal extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Toast.makeText(getApplicationContext(), "Sesion Cerrada", Toast.LENGTH_SHORT).show();
-                SharedPreferences prefs = getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE);
+               AlertDialog.Builder Dialogo =new AlertDialog.Builder(MenuPrincipal.this);
+                Dialogo.setTitle("Cerrar Sesion");
+                Dialogo.setMessage("Seguro que deseas salir?");
+                Dialogo.setIcon(R.drawable.delete);
 
-                SharedPreferences.Editor editor = prefs.edit();
-                editor.clear();
-                editor.commit();
-                Intent a = new Intent(getApplicationContext(), MainActivity.class);
-                finish();
-                startActivity(a);
+                Dialogo.setPositiveButton("Si",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(getApplicationContext(),"Sesion Cerrada",Toast.LENGTH_SHORT).show();
+                                Intent a =new Intent (getApplicationContext(),MainActivity.class);
+                                finish();
+                                startActivity(a);
+
+                            }});
+                Dialogo.setNegativeButton("No",
+                        new DialogInterface.OnClickListener(){
+                            public void onClick(DialogInterface dialog, int which){
+                                dialog.cancel();
+                            }
+                        });
+                Dialogo.show();
             }
             });
     }
