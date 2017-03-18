926fa225a245d6e8f7e540a5728d1f0f46ab41d1
CentralCornet8 <gabasteitor-300@hotmail.com>
2017-03-18 20:58:02 -0300

    gabo_Super_Titan

---
+++ app/src/main/java/com/example/gabriel/app_gabo/PolloMapas.java
@@ -0,0 +1,74 @@
+package com.example.gabriel.app_gabo;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class PolloMapas extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pollo_mapas);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+        int status= GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
+
+        if (status== ConnectionResult.SUCCESS) {
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+        }else{
+
+            Dialog dialog=GooglePlayServicesUtil.getErrorDialog(status,(Activity)getApplicationContext(),10);
+            dialog.show();
+        }
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        UiSettings uiSettings=mMap.getUiSettings();
+        uiSettings.setZoomControlsEnabled(true);
+
+        // Add a marker in Sydney and move the camera
+      /*  LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+
+        LatLng pollomulti = new LatLng(-16.510890424457667, -68.12183411267438);
+        mMap.addMarker(new MarkerOptions().position(pollomulti).title("Pollos Copacaba Multicine"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(pollomulti));
+
+        LatLng pollosoca = new LatLng(-16.494684744359958, -68.13585636612811);
+        mMap.addMarker(new MarkerOptions().position(pollosoca).title("Pollos Copacaba Socabaya"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(pollosoca));
+
+    }
+}
