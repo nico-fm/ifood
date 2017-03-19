20f05c4fd87c59379421a1d5a99ca5c077a9a4ad
personla <enrique.bayron.paz@gamail.com>
2017-03-19 02:00:45 -0400

    //aumentando codigo en elis //

--- app/src/main/java/com/example/gabriel/app_gabo/Elis.java
+++ app/src/main/java/com/example/gabriel/app_gabo/Elis.java
@@ -16,4 +16,41 @@ public class Elis extends AppCompatActivity {
     }
 }
 
+/**
+ *  private Button btnBuscarElis;
+ private Button btnPedidoElis;
+ private Button btnElisPlanet;
 
+
+
+ @Override
+ protected void onCreate(Bundle savedInstanceState) {
+ super.onCreate(savedInstanceState);
+ setContentView(R.layout.activity_Planet);
+
+ btnPedidoXpress=(Button)findViewById(R.id.btnPedidoElis);
+ btnBuscarMapaXpress=(Button)findViewById(R.id.btnBuscarMapaElis);
+ btnXpressPagina=(Button)findViewById(R.id.btnElisPagina);
+
+
+
+
+
+ btnXpressPagina.setOnClickListener(new View.OnClickListener() {
+ @Override
+ public void onClick(View view) {
+
+
+
+
+
+ Uri uriUrl = Uri.parse("http://www.elis.bo/");
+
+ Intent intent = new Intent(Intent.ACTION_VIEW, uriUrl);
+ startActivity(intent);
+
+ }
+ });
+ }
+ }
+ */
