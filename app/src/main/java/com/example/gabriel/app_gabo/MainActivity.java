b8459a427167ee564b13d2cd4c832e5d667b5dda
WINDOWS 8.1 <fabrizio.07.ff@gmail.com>
2017-03-20 15:57:20 -0400

    Commit

--- app/src/main/java/com/example/gabriel/app_gabo/MainActivity.java
+++ app/src/main/java/com/example/gabriel/app_gabo/MainActivity.java
@@ -22,7 +22,6 @@ import com.google.firebase.auth.FirebaseAuth;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    //:)
 
     private EditText txtUsuario;
     private EditText txtPassword;
