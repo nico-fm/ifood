5997477f0bd350bdb1f3a4bb3c3b67b9186a5729
HP <fpedronicolas@gmail.com>
2017-03-23 15:43:44 -0400

    subiendo antiguo proyecto otra vez

--- app/src/main/java/com/example/gabriel/app_gabo/Mrpizza.java
+++ app/src/main/java/com/example/gabriel/app_gabo/Mrpizza.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class Mrpizza extends AppCompatActivity {
-//hola
+//hola byronth ni codear ni chupar ni jugar futbol ni nada.. nada haces bien
 
     private Button btnBuscarMapaMr;
     private Button btnPedidoMr;
